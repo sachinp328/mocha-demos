79a1da0ac7c927a3f5de1b95bbf402c6e1cccc0d
Himanshu Ranjan <himanshur@lambdatest.com>
2022-06-16 00:59:12 +0530

    testing ftm with double commit-2

--- test/specs/factorialShort.spec.js
+++ test/specs/factorialShort.spec.js
@@ -23,7 +23,7 @@ describe('factorialShort', function() {
 
     it('should return 362880 when called with number 9', function() {
         // modifying test
-        // modifying test to run ftm
+        // modifying test to run ftm 2
         expect(factorial.factorialShort(9)).to.be.equal(362880);
     });
 
