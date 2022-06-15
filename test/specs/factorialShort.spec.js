ccf70c0cf96c9592d78506db055dba6a24964891
Himanshu Ranjan <himanshur@lambdatest.com>
2022-06-16 00:10:36 +0530

    testing ftm

--- test/specs/factorialShort.spec.js
+++ test/specs/factorialShort.spec.js
@@ -17,10 +17,12 @@ describe('factorialShort', function() {
     });
 
     it('should return 6 when called with number 3', function() {
+        // modifying test
         expect(factorial.factorialShort(3)).to.be.equal(6);
     });
 
     it('should return 362880 when called with number 9', function() {
+        // modifying test
         expect(factorial.factorialShort(9)).to.be.equal(362880);
     });
 
